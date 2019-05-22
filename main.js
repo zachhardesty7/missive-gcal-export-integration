cb35fbc6ea259ec31df2ce8e3943d857ad08210d
Zach Hardesty <zachhardesty7@gmail.com>
2019-05-22 17:03:39 -0500

    fix date conversion err, add blacklist, card style

--- main.js
+++ main.js
@@ -2,10 +2,22 @@
 // eslint-disable-next-line import/no-unresolved
 import { html, render } from 'https://unpkg.com/lit-html?module'
 
+const blacklist = [
+  '1-800'
+]
+
+const formatAsGCalDate = (str = '') => (
+  (str &&
+    (new Date(str)) &&
+    (new Date(str)).toISOString() &&
+    (new Date(str)).toISOString().replace(/-|:|\.\d\d\d/g, '')
+  ) || ''
+)
+
 const buildLink = (title = '', start = '', end = '', details = '', location = '') => {
   const link = new URLSearchParams()
-  const startFormatted = (new Date(start)).toISOString().replace(/-|:|\.\d\d\d/g, '')
-  const endFormatted = (new Date(end)).toISOString().replace(/-|:|\.\d\d\d/g, '')
+  const startFormatted = formatAsGCalDate(start)
+  const endFormatted = formatAsGCalDate(end)
 
   link.append('action', 'TEMPLATE')
   link.append('text', title)
@@ -19,22 +31,26 @@ const buildLink = (title = '', start = '', end = '', details = '', location = ''
   return `https://calendar.google.com/event?${link.toString()}`
 }
 
-const sidebar = (matches, title, details, location) => html`
-  <div class="text-large align-center padding-top-large">detected events</div>
-  ${matches.map((match) => {
-    const start = match.start.date()
-    const end = match.end.date()
-    const link = buildLink(title, start, end, details, location)
-
-    return item([match.text, start], link)
-  })}
-`
-
-const item = (rows = [], link) => html`
-  <div class="section padding-large">
-    ${rows.map(row => html`<p class="row">${row}</p>`)}
+const item = (orig, start = '', end = '', link) => html`
+  <div class="card shadow padding-xlarge">
+    <h3 class="title">${orig}</h3>
+    <div class="margin-top-xlarge margin-bottom-xlarge">
+      <p class="text">${start}</p>
+      ${end && html`<p class="text">${end}</p>`}
+    </div>
     <a target="_blank" href=${link} class="button">Export</a>
   </div>
+`
+
+const sidebar = (matches, title, details, location) => html`
+  <h2 class="text-xlarge align-center padding-top-medium">detected events</h2>
+  ${matches.map((match) => {
+    const start = match.start.date()
+    const end = match.end ? match.end.date() : ''
+    const link = buildLink(title, start, end, details, location)
+
+    return item(match.text, start, end, link)
+  })}
 `
 
 document.querySelector('#reload')
@@ -50,16 +66,19 @@ Missive.on('change:conversations', (ids) => {
           const template = document.createElement('template')
           template.innerHTML = message.body
           const nodes = template.content.firstChild
-          const text = nodes.innerText.replace(/\s+/gm, ' ').trim()
+          const body = nodes.innerText.replace(/\s+/gm, ' ').trim()
 
-          const raw = chrono.parse(text)
+          const matches = chrono.parse(body).filter(match => !blacklist.includes(match.text))
           const results = document.querySelector('#results')
 
-          raw.length !== 0
-            ? render(sidebar(raw, message.subject, `${text}\n${reference}`), results)
-            : render(html`<div class="text-large align-center padding-top-large">no matches</div>`, results)
+          matches.length !== 0
+            ? render(sidebar(matches, message.subject, `${reference}\n${body}`), results)
+            : render(html`<p class="text-large align-center padding-top-large">no matches</p>`, results)
         }
       }
       return null
-    }).catch(e => Missive.alert({ title: 'error in GCal script', message: e.toString() }))
+    }).catch((e) => {
+      console.error(`GCalError\n${e.stack}`)
+      Missive.alert({ title: 'error in GCal script', message: e.toString() })
+    })
 })
